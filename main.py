a32b65867c540e061e76913a8a1781cbe1872e66
unknown <welison-coder>
2022-06-08 11:34:31 -0300

    ativação de windows autamatização

---
+++ main.py
@@ -0,0 +1,136 @@
+import pyautogui
+
+
+# pyautogui.sleep(3)
+# pyautogui.press('winleft')
+# pyautogui.sleep(1)
+# pyautogui.write('\\\\172.16.0.4\chaves')
+# pyautogui.sleep(1)
+# pyautogui.press('enter')
+#
+# pyautogui.sleep(3)
+# pyautogui.write('dto')
+# pyautogui.write('2022')
+# pyautogui.sleep(1)
+# pyautogui.press(['tab', 'tab'])
+# pyautogui.press('enter')
+
+
+pyautogui.alert( '***Atenção***\n'
+                'Clique em [ok] para inicializar o programa.')
+pyautogui.press('winleft')
+pyautogui.sleep(3)
+pyautogui.write('C:')
+pyautogui.sleep(3)
+pyautogui.press('enter')
+
+
+pyautogui.sleep(3)
+pyautogui.hotkey('ctrl', 'f')
+pyautogui.sleep(3)
+pyautogui.write('PV 59117')
+pyautogui.sleep(3)
+pyautogui.press('enter')
+pyautogui.sleep(3)
+pyautogui.press('tab')
+pyautogui.sleep(3)
+pyautogui.press('space')
+pyautogui.sleep(3)
+pyautogui.press('enter')
+pyautogui.sleep(3)
+
+pyautogui.hotkey('ctrl', 'f')
+pyautogui.sleep(3)
+pyautogui.write('EMAILOFICE')
+pyautogui.sleep(3)
+pyautogui.press('enter')
+pyautogui.sleep(3)
+pyautogui.press('tab')
+pyautogui.sleep(3)
+pyautogui.press('space')
+pyautogui.sleep(3)
+pyautogui.press('enter')
+pyautogui.sleep(3)
+pyautogui.keyDown('ctrl')
+pyautogui.keyDown('shift')
+pyautogui.press('right', presses=1)
+pyautogui.keyUp('ctrl')
+pyautogui.keyUp('shift')
+pyautogui.hotkey('ctrl', 'c')
+pyautogui.sleep(3)
+pyautogui.press('backspace')
+pyautogui.sleep(3)
+pyautogui.press('down')
+pyautogui.sleep(3)
+pyautogui.press('backspace', presses=2)
+pyautogui.hotkey('ctrl','s')
+
+pyautogui.press('winleft')
+pyautogui.sleep(3)
+pyautogui.write('word')
+pyautogui.sleep(5)
+pyautogui.press('enter')
+pyautogui.sleep(6)
+pyautogui.press('tab')
+pyautogui.sleep(1)
+pyautogui.press('enter')
+pyautogui.sleep(16)
+pyautogui.press('tab')
+pyautogui.sleep(1)
+pyautogui.press('tab')
+pyautogui.sleep(1)
+pyautogui.press('enter')
+pyautogui.sleep(1)
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.sleep(1)
+pyautogui.keyDown('alt')
+pyautogui.press('tab', presses=2)
+pyautogui.keyUp('alt')
+pyautogui.sleep(1)
+pyautogui.keyDown('shift')
+pyautogui.keyDown('ctrl')
+pyautogui.keyDown('capslock')
+pyautogui.press('right', presses=1)
+pyautogui.keyUp('capslock')
+pyautogui.keyUp('ctrl')
+pyautogui.keyUp('shift')
+pyautogui.sleep(1)
+pyautogui.hotkey('ctrl','c')
+pyautogui.sleep(1)
+
+pyautogui.keyDown('alt')
+pyautogui.press('tab')
+pyautogui.keyUp('alt')
+pyautogui.sleep(1)
+pyautogui.hotkey('tab', presses=5)
+pyautogui.press('enter')
+pyautogui.sleep(2)
+
+
+
+
+
+
+
+
+#pyautogui.sleep(3)
+#pyautogui.hotkey('ctrl', 's')
+
+#pyautogui.sleep(3)
+#pyautogui.hotkey('alt','tab')
+
+
+
+#pyautogui.press('winleft')
+#pyautogui.sleep(3)
+#pyautogui.write('slui')
+#pyautogui.sleep(1)
+#pyautogui.press('right')
+#pyautogui.press('down')
+#pyautogui.sleep(3)
+#pyautogui.press('enter')
+#pyautogui.sleep(5)
+#pyautogui.press('enter')
+#pyautogui.sleep(5)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
